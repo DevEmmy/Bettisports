24953df9d9ae2aa03d8fab3797037c1a77e00449
web-tranquility <eolaosebikan60@gmail.com>
2024-07-06 16:49:37 +0100

    added real data top nav on dashboard

--- src/components/Dashboard/TopNav.tsx
+++ src/components/Dashboard/TopNav.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { getUser } from '@/hooks/UserRequests'
 import Image from 'next/image'
 import React from 'react'
 import { HiOutlineBell } from 'react-icons/hi'
@@ -5,18 +7,24 @@ import { HiChevronDown } from 'react-icons/hi2'
 import { RiBellLine } from 'react-icons/ri'
 
 const TopNav = () => {
+  const user = getUser()
+
+  if (!user) {
+    return null
+  }
+
   return (
     <div className='flex-center justify-between py-4 px-xPadding bg-white border-b fixed top-0 w-4/5 right-0'>
-        <p className='font-[600]'>Howdy, Williams</p>
+      <p className='font-[600]'>Howdy, {user.firstName}</p>
 
-        <div className='flex-center gap-20 '>
-            <HiOutlineBell size={24} />
+      <div className='flex-center gap-10 '>
+        <HiOutlineBell size={24} />
 
-            <div className='flex-center gap-5'>
-                <Image width={0} height={0} unoptimized src="/./img.jpg" alt="" className='pp' />
-                <HiChevronDown />
-            </div>
+        <div className='flex-center gap-5'>
+          <Image width={0} height={0} unoptimized src={user.profilePicture} alt="" className='pp' />
+          <HiChevronDown />
         </div>
+      </div>
     </div>
   )
 }
