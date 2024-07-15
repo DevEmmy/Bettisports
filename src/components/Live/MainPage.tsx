5299aa946dc0ddeedeee99accd2f0779a0f3b93b
web-tranquility <eolaosebikan60@gmail.com>
2024-07-15 18:06:10 +0100

    attempt to fix error

--- src/components/Live/MainPage.tsx
+++ src/components/Live/MainPage.tsx
@@ -48,7 +48,7 @@ const MainPage = () => {
                 </div>
                 <Tag />
 
-                <Comments />
+                {/* <Comments /> */}
 
                 {/* <CommentForm /> */}
             </div>
